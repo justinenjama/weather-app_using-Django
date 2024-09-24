184b13dd12744b77ebe3743fb4301ae1c3061970
unknown <justinenjama33@gmail.com>
2024-09-24 12:20:49 +0300

    second commit

---
+++ weatherdetector/weather/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import json
+import urllib.request
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        APIKEY = 'a6c0e2adf3d190c8df009a03616f0a21'
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKEY).read() # sending reguest to weather map
+        json_data = json.loads(res) # receiving the data from weather map as a json file
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' +
+            str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp'])+'K',
+            "pressure": str(json_data['main']['pressure']) + 'Pa',
+            "humidity": str(json_data['main']['humidity']) + '%',
+            "wind": str(json_data['wind']['speed']) + 'km/h',
+            "sea_level": str(json_data['main']['sea_level']) + 'Pa',
+            "visibility": str(json_data['visibility']) + 'm',
+            "cloudiness": str(json_data['clouds']['all']) + '%',
+            "weather_description": json_data['weather'][0]['description'],
+            "weather_icon": json_data['weather'][0].get('icon'),
+        }     
+    else:
+        city = ''
+        data = {}  # returning an empty dictionary
+    return render(request, 'index.html', {'city': city, 'data': data}) 
